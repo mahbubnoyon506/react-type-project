de639ad3b0e61b56a4af97567c29b23a724eba34
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-02-23 12:50:05 +0600

    todos added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,31 @@
 import React, { useState } from 'react';
 import InputBox from './InputBox';
+import { Todo } from './model';
 
-const Home : React.FC = () => {
+const Home: React.FC = () => {
     const [todo, setTodo] = useState<string>('')
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const handleAdd = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (todo) {
+            setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+            setTodo('')
+        }
+    }
+    console.log(todos)
     return (
         <div className=''>
             <h1 className='text-3xl md:text-4xl lg:text-5xl text-center font-bold uppercase pb-5 text-slate-100'>Taskify</h1>
-            <InputBox todo={todo} setTodo={setTodo} />
+            <InputBox todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+            <div>
+                {
+                    todos.map((item) => {
+                        return(
+                            <div>{item.todo}</div>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 };
--- src/pages/Home/InputBox.tsx
+++ src/pages/Home/InputBox.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-interface Props = {
-    todo: String;
-    setTodo: React.Dispatch<React.SetStateAction<string>>
+import React, { useRef } from 'react';
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputBox = ({todo, setTodo}: Props) => {
+const InputBox: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+   const inputRef = useRef<HTMLInputElement>(null)
     return (
         <div>
-            <form className='flex justify-center py-5'>
+            <form className='flex justify-center py-5' onSubmit={(e) => {
+                handleAdd(e)
+                inputRef.current?.blur()
+                }} >
                 <div className='flex items-center relative'>
-                    <input className='py-5 px-10 w-full md:w-96 rounded-full focus:outline-none text-lg text-slate-900' type="text" name="todoName" id="" placeholder='Type your todo' />
-                    <button className='bg-slate-900 rounded-full ml-[-130px] py-2 px-5 text-slate-100'>Add to List</button>
+                    <input className='py-5 px-10 w-full md:w-96 rounded-full focus:outline-none text-lg text-slate-900' ref={inputRef} value={todo} onChange={(e) => setTodo(e.target.value)} type="text" name="todoName" id="" placeholder='Type your todo' />
+                    <button className='bg-slate-900 rounded-full ml-[-130px] py-2 px-5 text-slate-100' type="submit" >Add to List</button>
                 </div>
             </form>
         </div>
