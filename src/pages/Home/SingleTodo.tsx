7e41a54759a26c6068ef61c7b66df7bc827f0471
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-02-24 12:03:53 +0600

    todo adding, deletion and edit state added

--- src/pages/Home/SingleTodo.tsx
+++ src/pages/Home/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Todo } from './model';
 import { AiFillEdit, AiOutlineCheck } from 'react-icons/ai';
 import { FaTrashAlt } from 'react-icons/fa';
@@ -10,22 +10,53 @@ type Props = {
 }
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+    const [edit, setEdit] = useState<boolean>(false);
+
+    const [editTodo, setEditTodo] = useState<string>(todo.todo)
 
     const handleTodoDone = (id: number) => {
         setTodos(todos.map(todo => todo.id === id ? { ...todo, isDone: !todo.isDone } : todo))
     }
 
+    const handleDelete = (id: number) => {
+        setTodos(todos.filter((todo) => todo.id !== id))
+    }
+
+    const handleSubmit = (e: React.FormEvent, id:number) => {
+       e.preventDefault();
+       setTodos(todos.map((todo) => (todo.id === id ? {...todo, todo:editTodo} : todo)));
+       setEdit(false)
+    }
+
+    const inputRef = useRef<HTMLInputElement>(null);
+    useEffect( () => {
+      inputRef.current?.focus()
+    }, [edit])
+
     return (
         <div className='flex justify-between items-center bg-gray-700 rounded shadow-lg p-5'>
             {
-                todo.isDone ?
-                    <s className='bg-transparent focus:outline-none text-slate-100'> {todo.todo} </s> :
-                    <span className='bg-transparent focus:outline-none text-slate-100'>{todo.todo} </span>
+                edit ? (
+                    <form className='flex justify-center items-center relative' onSubmit={ (e) => handleSubmit( e, todo.id)}>
+                        <input ref={inputRef} className='w-96 py-2 pl-2 focus:outline-none rounded shadow-lg' type="text" name="" id="" value={editTodo} onChange={ (e) => setEditTodo(e.target.value) } />
+                        <button className='uppercase px-2 rounded bg-slate-900 text-slate-100 text-lg absolute right-2'>Edit</button>
+                    </form>
+                ) :
+                    (
+                        todo.isDone ?
+                            <s className='bg-transparent focus:outline-none text-slate-100'> {todo.todo} </s> :
+                            <span className='bg-transparent focus:outline-none text-slate-100'>{todo.todo} </span>
+                    )
+
             }
             <div className='flex items-center text-slate-100'>
-                <AiFillEdit className='cursor-pointer' size={30} />
-                <AiOutlineCheck className='cursor-pointer mx-2' onClick={() => handleTodoDone(todo.id)} size={30}  />
-                <FaTrashAlt className='cursor-pointer' size={25} />
+                <AiFillEdit className='cursor-pointer' size={30} onClick={() => {
+                    if (!edit && !todo.isDone) {
+                        setEdit(!edit)
+                    }
+                }} />
+                <AiOutlineCheck className='cursor-pointer mx-2' onClick={() => handleTodoDone(todo.id)} size={30} />
+                <FaTrashAlt className='cursor-pointer' onClick={() => handleDelete(todo.id)} size={25} />
             </div>
         </div>
     );
